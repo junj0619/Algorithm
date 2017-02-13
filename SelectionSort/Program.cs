5700d4be19cb20e7c87247f0b8beeb17c181a19a
junj0619 <junj0619@gmail.com>
2017-02-13 09:57:42 -0500

    SelectionSort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = SelectionSort(new int[] { 54, 23, 1, 66, 90, 100, 4 });
+            foreach (var element in array)
+                Console.WriteLine(element);
+        }
+
+
+        public static int[] SelectionSort(int[] array)
+        {
+
+            int min = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                        min = j;
+                }
+                int temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+
+            return array;
+        }
+    }
+}
