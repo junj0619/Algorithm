35bc0f2eb3352c910b4f4903475ca60915a9611f
Jun <Jun@JUN-PC>
2017-02-16 21:23:22 -0500

    check in merg sort

---
+++ MergSort/MergSort.cs
@@ -0,0 +1,63 @@
+﻿namespace MergSort
+{
+    public class MergSort
+    {
+        private static int[] _tempArray;
+
+        public MergSort(int length)
+        {
+            _tempArray = new int[length];
+        }
+
+        public static void Sort(ref int[] inputArray)
+        {
+            Sort(ref inputArray, 0, inputArray.Length - 1);
+        }
+
+        private static void Sort(ref int[] inputArray, int start, int end)
+        {
+            if (end <= start) return;
+
+            int mid = (start + end) / 2;
+            Sort(ref inputArray, start, mid);
+            Sort(ref inputArray, mid + 1, end);
+            Merg(ref inputArray, start, mid, end);
+        }
+
+        public static void Merg(ref int[] inputArray, int start, int mid, int end)
+        {
+            int leftStart = start;
+            int rightStart = mid + 1;
+
+            for (int k = 0; k <= end; k++)
+                _tempArray[k] = inputArray[k];
+
+            for (int k = start; k <= end; k++)
+            {
+                if (leftStart > mid)
+                {
+                    inputArray[k] = _tempArray[rightStart];
+                    rightStart++;
+                }
+                else if (rightStart > end)
+                {
+                    inputArray[k] = _tempArray[leftStart];
+                    leftStart++;
+                }
+                else if (_tempArray[leftStart] > _tempArray[rightStart])
+                {
+                    inputArray[k] = _tempArray[rightStart];
+                    rightStart++;
+                }
+                else
+                {
+                    inputArray[k] = _tempArray[leftStart];
+                    leftStart++;
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ MergSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 9, 7, 3, 1, 6, 3, 2, 6, 8, 9, 3, 0 };
+            var sorter = new MergSort(inputArray.Length);
+
+            MergSort.Sort(ref inputArray);
+
+            for (int i = 0; i < inputArray.Length; i++)
+                Console.WriteLine(inputArray[i]);
+        }
+    }
+}
